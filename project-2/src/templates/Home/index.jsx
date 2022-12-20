8cd5f009ce37a616a1894150183195f6ddff15ad
feliperuppel30 <115996060+feliperuppel30@users.noreply.github.com>
2022-12-19 21:58:56 -0300

    estudos react

---
+++ project-2/src/templates/Home/index.jsx
@@ -0,0 +1,78 @@
+import "./styles.css";
+
+import { Component } from "react";
+
+import { loadPosts } from "../../utils/load-posts";
+import { Posts } from "../../components/posts";
+import { Button } from "../../components/Button";
+
+class Home extends Component {
+  state = {
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 10,
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+  loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
+    const postsAndPhotos = await loadPosts();
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    });
+  };
+
+  loadMorePosts = () => {
+    const { page, postsPerPage, allPosts, posts } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
+  };
+
+  render() {
+    const { posts, page, postsPerPage, allPosts } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    return (
+      <section className="container">
+        <Posts posts={posts} />
+        <div className="button-container">
+          <Button
+            text="Load more Posts"
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+          />
+        </div>
+      </section>
+    );
+  }
+}
+
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
+export default Home;
