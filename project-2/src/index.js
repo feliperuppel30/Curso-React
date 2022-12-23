38b956124f6b8d840a9ba750b797e3104c4a4205
feliperuppel30 <115996060+feliperuppel30@users.noreply.github.com>
2022-12-23 13:52:50 -0300

    correçoes

--- project-2/src/index.js
+++ project-2/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./styles/global-styles.css";
+
 import Home from "./templates/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
